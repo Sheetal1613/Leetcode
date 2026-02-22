250c963120684852e084c7f40e856148bfd99a45
Sheetal <sheetal13013@gmail.com>
2026-02-22 11:24:35 +0530

    Time: 5 ms (64.67%) | Memory: 42.7 MB (11.96%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -15,7 +15,7 @@ class Solution {
         if (n<=1) {
             return false;
         }
-        for (int i=2;i*i <= n;i++) {
+        for (int i=2;i < n;i++) {
             if (n%i==0) {
                 return false;
             }
