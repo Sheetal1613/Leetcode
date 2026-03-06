5bf60f3724ba57c05ca68935c26b1732cb187513
Sheetal <sheetal13013@gmail.com>
2026-03-06 18:15:54 +0530

    Time: 15 ms (64.99%) | Memory: 93.4 MB (56.89%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -9,18 +9,29 @@ class Solution {
         // return false;      
 
         //using hash table
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int num : nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-            if(map.get(num)>1){
-                return true;
-            }
-        }
+        // Map<Integer,Integer> map = new HashMap<>();
+        // for(int num : nums){
+        //     map.put(num,map.getOrDefault(num,0)+1);
+        //     if(map.get(num)>1){
+        //         return true;
+        //     }
+        // }
         // for(Map.Entry<Integer,Integer> entry : map.entrySet()){
         //     if(entry.getValue()>1){
         //         return true;
         //     }
         // }
+          // return false;
+
+          Set<Integer> set = new HashSet<>();
+           for(int i=0; i<nums.length; i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            else{
+                set.add(nums[i]);
+            }
+           }
            return false;
     }
 }
