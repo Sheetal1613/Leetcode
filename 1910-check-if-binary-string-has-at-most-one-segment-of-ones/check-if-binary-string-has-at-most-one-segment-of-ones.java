5514dd4a18fc5fda716f0f27f83ae58433eb839b
Sheetal <sheetal13013@gmail.com>
2026-03-06 10:54:41 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (50.94%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.java
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -1,21 +1,22 @@
 class Solution {
     public boolean checkOnesSegment(String s) {
-    
-        int index = -1;
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) == '0'){
-               index = i;
-               break;
-            }
-        }
-        if(index == -1){
-            return true;
-        }
-        for(int i = index; i<s.length()-1; i++){
-            if(s.charAt(i+1) == '1'){
-                 return false;
-            }
-        }
-        return true;
+        // int index = -1;
+        // for(int i=0; i<s.length(); i++){
+        //     if(s.charAt(i) == '0'){
+        //        index = i;
+        //        break;
+        //     }
+        // }
+        // if(index == -1){
+        //     return true;
+        // }
+        // for(int i = index; i<s.length()-1; i++){
+        //     if(s.charAt(i+1) == '1'){
+        //          return false;
+        //     }
+        // }
+        // return true;
+
+        return !s.contains("01");
     }
 }
