522a7f9a45f88fee66dc2bf6c1209871ec28231a
Sheetal <sheetal13013@gmail.com>
2026-03-06 16:17:27 +0530

    Time: 36 ms (5.08%) | Memory: 95.9 MB (22.62%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        // Arrays.sort(nums);  
+        // for (int i = 1; i < nums.length; i++) {
+        //     if (nums[i] == nums[i - 1]) {
+        //         return true;
+        //     }
+        // } 
+        // return false;      
+
+        //using hash table
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(entry.getValue()>1){
+                return true;
+            }
+        }
+           return false;
+    }
+}
