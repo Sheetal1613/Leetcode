22fd18e8b8b60a9d4bc030aebd2d94f30050699f
Sheetal <sheetal13013@gmail.com>
2026-03-29 23:24:43 +0530

    Time: 1 ms (100.00%) | Memory: 44.2 MB (68.88%) - LeetSync

---
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean canBeEqual(String s1, String s2) {
+        if(s1.equals(s2)){
+            return true;
+        }
+        char[] str1 = s1.toCharArray();
+        char[] str2 = s2.toCharArray();
+        // for(int i=0; i<2; i++){
+        //     if(s1.charAt(i)!=s2.charAt(i)){
+        //         char temp = s2.charAt(i);
+        //         s2.charAt[i] = s2.charAt(i+2;
+        //         s2.charAt(i+2) = temp;
+        //     }
+        //     if(s1.equals(s2)){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        for(int i=0; i<2; i++){
+            if(str1[i] != str2[i]){
+                   char temp = str2[i];
+                   str2[i] = str2[i+2];
+                   str2[i+2] = temp;
+            }
+            if(Arrays.equals(str1,str2)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
