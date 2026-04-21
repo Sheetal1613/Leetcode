8e02732859f6040cf433ad2af8b02276662b0c47
Sheetal <sheetal13013@gmail.com>
2026-04-21 23:10:58 +0530

    Time: 5 ms (38.85%) | Memory: 43 MB (99.08%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder str = new StringBuilder();
+        int count = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == ')'){
+                count--;
+            }
+            if(count != 0){
+              str = str.append(s.charAt(i));
+            }
+            if(s.charAt(i) == '('){
+                count++;
+            }
+        }
+        return str.toString();
+    }
+}
