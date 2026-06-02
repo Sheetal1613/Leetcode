d44087e39236c7ffd135d81eafcc2f329f9e74d9
Sheetal <sheetal13013@gmail.com>
2026-06-02 18:27:52 +0530

    Time: 3 ms (86.69%) | Memory: 52 MB (76.02%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -7,37 +7,56 @@ class Solution {
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(!vis[i][j] && grid[i][j] == '1'){
-                    bfs(i,j,vis,grid,m,n);
+                   // bfs(i,j,vis,grid,m,n);
+                   dfs(i, j, vis, grid);
                     island++;
                 }
             }
         }
         return island;
     }
-        public void bfs(int i, int j, boolean[][] vis, char[][] grid, int m, int n){
-          Queue<int[]> q = new LinkedList<>();
-          q.add(new int[]{i,j});
-          vis[i][j] = true;
-          while(!q.isEmpty()){
-            int[] front = q.poll();
-            int a = front[0];
-            int b = front[1];
-                if((a-1)>= 0 && !vis[a-1][b] && grid[a-1][b] == '1'){
-                    q.offer(new int[]{a-1,b});
-                    vis[a-1][b] = true;
-                }
-                if((b-1) >=0 && !vis[a][b-1] && grid[a][b-1] == '1'){
-                    q.offer(new int[]{a,b-1});
-                    vis[a][b-1] = true;
-                }
-                if((a+1) < m && !vis[a+1][b] && grid[a+1][b] == '1'){
-                    q.offer(new int[]{a+1,b});
-                    vis[a+1][b] = true;
-                }
-                if((b+1) < n && !vis[a][b+1] && grid[a][b+1] == '1'){
-                    q.offer(new int[]{a,b+1});
-                    vis[a][b+1] = true;
-                }
-            }
+            
+        public void dfs(int i, int j, boolean[][] vis, char[][] grid){
+            int m = grid.length;
+            int n = grid[0].length;
+            vis[i][j] = true;
+            if(i-1 >= 0 && !vis[i-1][j] && grid[i-1][j] == '1')
+            dfs(i-1, j, vis, grid);
+            if(j-1 >= 0 && !vis[i][j-1] && grid[i][j-1] == '1')
+            dfs(i, j-1, vis, grid);
+            if(i+1 < m && !vis[i+1][j] && grid[i+1][j] == '1')
+            dfs(i+1, j, vis, grid);
+            if(j+1 < n && !vis[i][j+1] && grid[i][j+1] == '1')
+            dfs(i, j+1, vis, grid);
         }
+         
+
+        // public void bfs(int i, int j, boolean[][] vis, char[][] grid, int m, int n){
+        //   Queue<int[]> q = new LinkedList<>();
+        //   q.add(new int[]{i,j});
+        //   vis[i][j] = true;
+        //   while(!q.isEmpty()){
+        //     int[] front = q.poll();
+        //     int a = front[0];
+        //     int b = front[1];
+        //         if((a-1)>= 0 && !vis[a-1][b] && grid[a-1][b] == '1'){
+        //             q.offer(new int[]{a-1,b});
+        //             vis[a-1][b] = true;
+        //         }
+        //         if((b-1) >=0 && !vis[a][b-1] && grid[a][b-1] == '1'){
+        //             q.offer(new int[]{a,b-1});
+        //             vis[a][b-1] = true;
+        //         }
+        //         if((a+1) < m && !vis[a+1][b] && grid[a+1][b] == '1'){
+        //             q.offer(new int[]{a+1,b});
+        //             vis[a+1][b] = true;
+        //         }
+        //         if((b+1) < n && !vis[a][b+1] && grid[a][b+1] == '1'){
+        //             q.offer(new int[]{a,b+1});
+        //             vis[a][b+1] = true;
+        //         }
+        //     }
+        // }
+
+
 }
